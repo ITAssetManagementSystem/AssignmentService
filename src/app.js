aedce3a5634a4027fee59e56085201b97b05b295
Nilay Saxena <nilaysaxena94@gmail.com>
2026-01-02 08:48:32 +0000

    Ingress Changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.use('/assignments', assignmentRoutes);
 
 module.exports = app;
